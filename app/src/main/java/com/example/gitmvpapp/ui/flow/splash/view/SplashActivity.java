1a80f37057e6e422031c984c8ca4f31a274edf9d
Dandelion <alexdandelion2015@gmail.com>
2020-09-24 16:05:02 +0300

    - created base app structure; - created app first screen; - added: fonts, colors, strings, styles;

---
+++ app/src/main/java/com/example/gitmvpapp/ui/flow/splash/view/SplashActivity.java
@@ -0,0 +1,16 @@
+package com.example.gitmvpapp.ui.flow.splash.view;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.gitmvpapp.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
