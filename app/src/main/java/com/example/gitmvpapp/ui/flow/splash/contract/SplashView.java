8f1a015da83c15f7e6305abf3af7ad90b1ed2531
Dandelion <alexdandelion2015@gmail.com>
2020-09-28 12:21:01 +0300

    - implemented "Splash" flow;

---
+++ app/src/main/java/com/example/gitmvpapp/ui/flow/splash/contract/SplashView.java
@@ -0,0 +1,10 @@
+package com.example.gitmvpapp.ui.flow.splash.contract;
+
+import com.example.gitmvpapp.ui.flow.base.contract.BaseView;
+
+public interface SplashView extends BaseView {
+
+    void openLoginScreen();
+
+    void openMainScreen();
+}
--- app/src/main/java/com/example/gitmvpapp/ui/flow/splash/view/SplashActivity.java
+++ app/src/main/java/com/example/gitmvpapp/ui/flow/splash/view/SplashActivity.java
@@ -1,16 +1,68 @@
 package com.example.gitmvpapp.ui.flow.splash.view;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.LifecycleObserver;
 
+import com.arellomobile.mvp.presenter.InjectPresenter;
 import com.example.gitmvpapp.R;
+import com.example.gitmvpapp.di.component.ApplicationComponent;
+import com.example.gitmvpapp.di.component.DaggerSplashActivityComponent;
+import com.example.gitmvpapp.di.module.SplashActivityModule;
+import com.example.gitmvpapp.ui.flow.base.activity.BaseActivity;
+import com.example.gitmvpapp.ui.flow.splash.contract.SplashView;
+import com.example.gitmvpapp.ui.flow.splash.presenter.SplashPresenter;
 
-public class SplashActivity extends AppCompatActivity {
+import timber.log.Timber;
+
+public class SplashActivity extends BaseActivity implements SplashView {
+
+    @InjectPresenter
+    SplashPresenter presenter;
+
+    public static Intent createClearIntent(Context context) {
+        return new Intent(context, SplashActivity.class)
+                .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+    }
+
+    @Override
+    protected LifecycleObserver getLifecycleObserver() {
+        return presenter;
+    }
+
+    @Override
+    protected void initDependencies(ApplicationComponent applicationComponent) {
+        DaggerSplashActivityComponent.builder().applicationComponent(applicationComponent)
+                .splashActivityModule(new SplashActivityModule()).build().inject(presenter);
+    }
+
+    @Override
+    public void openLoginScreen() {
+        startActivity(LoginActivity.createClearIntent(this));
+        finish();
+    }
+
+    @Override
+    public void openMainScreen() {
+        startActivity(MainActivity.createIntent(this));
+        finish();
+    }
+
+    @Override
+    public void updateLoading(boolean show) {
+        Timber.i("Update loading");
+    }
+
+    @Override
+    public void showError(String message) {
+        showDialog().info(message);
     }
 }
